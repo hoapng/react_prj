c8e9c108a72fec7e06814b2c8c34dfe6a39e20b0
hoapng <giahoa01giathieu@gmail.com>
2023-04-05 09:22:45 +0700

    set up sate value

--- src/context.jsx
+++ src/context.jsx
@@ -4,7 +4,8 @@ import sublinks from './data'
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) =>{
-
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isModalOpen, setIsModalOpen] = useState(true);
 
     return <AppContext.Provider>
         {children}
