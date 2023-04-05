9df15ccf97daec14817f57bc68b3a535770afcbf
hoapng <giahoa01giathieu@gmail.com>
2023-04-05 09:35:25 +0700

    update and fix sate value

--- src/context.jsx
+++ src/context.jsx
@@ -5,9 +5,29 @@ const AppContext = React.createContext();
 
 const AppProvider = ({children}) =>{
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const [isModalOpen, setIsModalOpen] = useState(true);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
 
-    return <AppContext.Provider>
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    };
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    };
+    const openSubmenu = () => {
+        setIsSubmenuOpen(true);
+    };
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false);
+    };
+
+    return <AppContext.Provider value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+      }}>
         {children}
     </AppContext.Provider>
 }
