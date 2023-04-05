90fb5c25a41f79b2f0448126275f1f0c798df1eb
hoapng <giahoa01giathieu@gmail.com>
2023-04-05 09:11:47 +0700

    create context.jsx const

--- src/context.jsx
+++ src/context.jsx
@@ -1,3 +1,12 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
 
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) =>{
+
+
+    return <AppContext.Provider>
+        {children}
+    </AppContext.Provider>
+}
