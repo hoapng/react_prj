2414d9e328a5fdf149dfe952155f2076943996e7
hoapng <giahoa01giathieu@gmail.com>
2023-04-06 20:48:34 +0700

    sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 import sublinks from './data'
+import { useGlobalContext } from './context'
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+  return (
+    <aside
+      className={`${
+        isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+      }`}
+    ></aside>
+  )
 }
 
 export default Sidebar
